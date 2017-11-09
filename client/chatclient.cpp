d83127c4fd9e755d7f73c413ce9bd3887f535e8e
Patrick James Akalaitis Falvey <pfalvey@nd.edu>
2017-11-08 21:16:53 -0500

    make client into a client

--- client/chatclient.cpp
+++ client/chatclient.cpp
@@ -3,72 +3,94 @@
     Compile
 */
  
-#include<stdio.h>
-#include<string.h>    //strlen
-#include<stdlib.h>    //strlen
-#include<sys/socket.h>
 #include<arpa/inet.h> //inet_addr
-#include<unistd.h>    //write
 #include<pthread.h> //for threading , link with lpthread
- 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <sstream>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
 //the thread function
 void *connection_handler(void *);
  
 int main(int argc , char *argv[])
 {
-    int socket_desc , client_sock , c;
-    struct sockaddr_in server , client;
-     
-    //Create socket
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1)
+    int SERVER_PORT;
+    FILE *fp;
+    struct hostent *hp;
+    struct sockaddr_in sin;
+    char *host;
+    char *username;
+    char buf[BUFSIZ];
+    int s;
+    int len;
+    std::string response;
+    if (argc==4) 
     {
-        printf("Could not create socket");
+        host = argv[1];
+        SERVER_PORT = atoi(argv[2]);
+        username = argv[3];
     }
-    puts("Socket created");
-     
-    //Prepare the sockaddr_in structure
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( 8888 );
-     
-    //Bind
-    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    else 
     {
-        //print the error message
-        perror("bind failed. Error");
-        return 1;
+        fprintf(stderr, "usage: ./myftp [SERVER NAME] [PORT]\n");
+        exit(1);
     }
-    puts("bind done");
-     
-    //Listen
-    listen(socket_desc , 3);
-     
-    //Accept and incoming connection
-    puts("Waiting for incoming connections...");
-    c = sizeof(struct sockaddr_in);
-     
-    //Accept and incoming connection
-    puts("Waiting for incoming connections...");
-    c = sizeof(struct sockaddr_in);
+
+    /* translate host name into peer's IP address */
+    hp = gethostbyname(host);
+    if (!hp) 
+    {
+        fprintf(stderr, "simplex-talk: unknown host: %s\n", host);
+        exit(1);
+    }
+
+    /* build address data structure */
+    bzero((char *)&sin, sizeof(sin));
+    sin.sin_family = AF_INET;
+    bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+    sin.sin_port = htons(SERVER_PORT);
+    /* active open */
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) 
+    {
+        perror("simplex-talk: socket"); exit(1);
+    }
+    
+    printf("Welcome to the TCP client!\n");
+
+    if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+    {
+      perror("simplex-talk: connect");
+      close(s); exit(1);
+    }
+
     pthread_t thread_id;
     
-    while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+    puts("Connected to server\n"); 
+         
+    if( pthread_create( &thread_id , NULL ,  connection_handler , (void*) &s) < 0)
     {
-        puts("Connection accepted");
-         
-        if( pthread_create( &thread_id , NULL ,  connection_handler , (void*) &client_sock) < 0)
-        {
-            perror("could not create thread");
-            return 1;
-        }
-         
-        //Now join the thread , so that we dont terminate before the thread
-        //pthread_join( thread_id , NULL);
-        puts("Handler assigned");
+        perror("could not create thread");
+        return 1;
     }
      
-    if (client_sock < 0)
+    //Now join the thread , so that we dont terminate before the thread
+    //pthread_join( thread_id , NULL);
+    puts("Handler assigned");
+     
+    if (s < 0)
     {
         perror("accept failed");
         return 1;
@@ -85,26 +107,41 @@ void *connection_handler(void *socket_desc)
     //Get the socket descriptor
     int sock = *(int*)socket_desc;
     int read_size;
-    char client_message[2000];
      
     //Send some messages to the client
-    char message[BUFSIZ] = "Greetings! I am your connection handler\n";
-    write(sock , message , strlen(message));
+    char buf[BUFSIZ];;
      
-    sprintf(message,"Now type something and i shall repeat what you type \n");
-    write(sock , message , strlen(message));
      
+    if (read_size = recv(sock , buf, sizeof(buf) , 0) == -1){
+        perror("Error receiving message from server\n");
+        exit(1);
+
+    }
+    std::cout<<buf<<std::endl;
+    memset(buf, 0, sizeof(buf));
+    if (read_size = recv(sock , buf, sizeof(buf) , 0) == -1){
+        perror("Error receiving message from server\n");
+        exit(1);
+
+    }
+    std::cout<<buf<<std::endl;
+    memset(buf, 0, sizeof(buf));
     //Receive a message from client
-    while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
+    while( fgets(buf, sizeof(buf), stdin))
     {
-        //end of string marker
-        client_message[read_size] = '\0';
+        buf[strlen(buf)] = '\0';
         
-        //Send the message back to client
-        write(sock , client_message , strlen(client_message));
-        
+        //Send the message to server
+        write(sock , buf , strlen(buf));
+        memset(buf, 0, sizeof(buf));
+        //read message from server
+        if (read_size = recv(sock , buf, sizeof(buf) , 0) == -1){
+            perror("Error receiving message from server\n");
+            exit(1);
+        }
+        std::cout<<buf<<std::endl;
         //clear the message buffer
-        memset(client_message, 0, 2000);
+        memset(buf, 0, sizeof(buf));
     }
      
     if(read_size == 0)
